c4b1959f1da4c856d42042700b12adceee422319
Ben Stone <benjamin.v.stone@gmail.com>
2015-01-10 16:52:50 -0800

    further update of scaffolding files

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -1,10 +1,3 @@
-/**
- * @ngdoc function
- * @name ticTacToeApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the ticTacToeApp
- */
 angular.module('ticTacToeApp')
   .controller('GameCtrl', function () {
 
