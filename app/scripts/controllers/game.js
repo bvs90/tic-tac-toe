1e70b4781c0e5dbe2c084a36542b583cbb4fe61b
Ben Stone <benjamin.v.stone@gmail.com>
2015-01-14 20:02:33 -0800

    Begin playerMove method with unit tests

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -5,14 +5,20 @@ angular.module('ticTacToeApp')
     // initialise a board for the game
     game.board = BoardSvc.createBoard();
     
+    // game.status;
+    
+    
     console.log('board', game.board);
     
     // function to mark player move and update view 
     game.playerMove = function(square) {
       square.value = 'X';
-    };
-    
     // then check if player has won
+      if (BoardSvc.scanBoardForWin(game.board, 'X') === true) {
+        game.status = 'Player has won!';
+      }
     // then run opponent turn function
     // check if opponent has won
+    };
+    
   });
--- test/spec/controllers/game_spec.js
+++ test/spec/controllers/game_spec.js
@@ -3,19 +3,55 @@ describe('Controller: GameCtrl', function () {
   beforeEach(module('ticTacToeApp'));
 
   var Game,
+    mockBoardSvc,
     scope;
+
+
+    mockBoardSvc = {
+      scanBoardForWin: angular.noop,
+      createBoard: function() {
+        return [];
+      }
+    };
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
     Game = $controller('GameCtrl', {
-      $scope: scope
+      $scope: scope,
+      BoardSvc: mockBoardSvc
     });
   }));
   
   it('should initialise a game board', function () {
-    expect(Game.board).toBeDefined();
+    expect(Array.isArray(Game.board)).toBe(true);
   });
 
+  describe('playerMove method', function() {
+    it('should update the value of the selected square', function() {
+      var mockSquare = [{value:null}];
+      Game.playerMove(mockSquare);
+      
+      expect(mockSquare.value).toBe('X');
+    });
+    
+    it('should check if the player has won', function() {
+      spyOn(mockBoardSvc, 'scanBoardForWin');
+      
+      var mockSquare = [{value:null}];
+      Game.playerMove(mockSquare);
+      
+      expect(mockBoardSvc.scanBoardForWin).toHaveBeenCalled();
+    });
+    
+    it('should update the game status if the player has won', function() {
+      spyOn(mockBoardSvc, 'scanBoardForWin').and.returnValue(true);
+      
+      var mockSquare = [{value:null}];
+      Game.playerMove(mockSquare);
+      
+      expect(Game.status).toBe('Player has won!');
+    });
+  });  
 
 });
